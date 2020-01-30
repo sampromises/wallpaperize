ded3f518c1ecf816771289cf2eaf620e42baabfd
Sam Kim <sam@sskymail.com>
2020-01-29 20:20:44 -0500

    Add hello world view

--- converter/views.py
+++ converter/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    template = loader.get_template('converter/index.html')
+    context = {
+        'name': 'Sam Kim',
+    }
+    return HttpResponse(template.render(context, request))
